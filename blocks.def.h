e58598cc3f29ddb471e33dff8b599b4f926c4b27
Tim Walter <techtim02@gmail.com>
2022-08-27 01:13:40 +0200

    added initial files

---
+++ blocks.def.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"", "date +'%d %b %j %H:%M:%S'",					5,		0},
+	{"dwm 6.3", "", 							0, 		0},
+	{"OpenBSD ", "uname -r", 						0, 		0}
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
